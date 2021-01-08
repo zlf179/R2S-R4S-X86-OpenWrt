6be9fba2825791b9bfcc9424c5b6a67de93935d8
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2021-01-08 19:18:10 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -342,6 +342,8 @@ svn co https://github.com/openwrt/packages/trunk/libs/libcap-ng feeds/packages/l
 ln -sf ../../../feeds/packages/libs/libcap-ng ./package/feeds/packages/libcap-ng
 rm -rf ./feeds/packages/utils/collectd
 svn co https://github.com/openwrt/packages/trunk/utils/collectd feeds/packages/utils/collectd
+svn co https://github.com/openwrt/packages/trunk/utils/hwdata feeds/packages/utils/hwdata
+ln -sf ../../../feeds/packages/utils/hwdata ./package/feeds/packages/hwdata
 #ipv6-helper
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/lean/ipv6-helper
 #IPSEC
